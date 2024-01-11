da1d75309217491064320a974cd19204c09194bb
favud <favud20@gmail.com>
2024-01-11 19:37:49 +0900

    Create Project

---
+++ src/test/java/com/sk/skmall/SkmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.skmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
